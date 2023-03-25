5904994df267b665e0a673b736511e7f393e4aaf
Vaishnavi Kale <vaishnavi.a.kale@cumminscollege.in>
2023-03-25 08:45:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const registrationForm = document.getElementById('registrationForm');
+
+registrationForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(registrationForm);
+  const vendorName = formData.get('vendorName');
+  const vendorEmail = formData.get('vendorEmail');
+  const vendorPhone = formData.get('vendorPhone');
+  const vendorAddress = formData.get('vendorAddress');
+
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      vendorName: vendorName,
+      vendorEmail: vendorEmail,
+      vendorPhone: vendorPhone,
+      vendorAddress: vendorAddress,
+    })
+  };
+
+  fetch('https://example.com/api/vendors', requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      alert('Registration Successful!');
+      registrationForm.reset();
+    })
+    .catch(error => console.error(error));
+});
